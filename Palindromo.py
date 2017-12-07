d8e21918789a0893a6a45bd1e10523e64afb6611
esej-imras <34324919+esej-imras@users.noreply.github.com>
2017-12-07 10:41:32 -0600

    Update Palindromo.py

--- Palindromo.py
+++ Palindromo.py
@@ -3,13 +3,13 @@
 # UNAM-CERT
 
 def palindrome():
-"""
+	"""
 Funcion que lee desde terminal una palabra y determina si es un palindromo
 comparando que la cadena original sea igual a la cadena inversa que se obtiene con un slice a la lista.
 
 Function that reads from terminal a word and determines if it is a palindrome
 comparing that the original string is equal to the reverse string obtained with a slice to the list.
-"""
+	"""
 	b=raw_input()
 	c=b[::-1]
 	if b==c:
